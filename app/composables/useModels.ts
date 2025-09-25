d488644ceba42b8a4d2c7d0b01d132eba28fa136
Gabriel Serejo <gabriel_serejo@live.com>
2025-09-25 09:12:07 -0300

    feat: added new supported models

--- app/composables/useModels.ts
+++ app/composables/useModels.ts
@@ -1,10 +1,17 @@
 export function useModels() {
   const models = [
     'gemini-2.5-pro',
-    'gemini-2.5-flash'
+    'gemini-2.5-flash',
+    'gemini-2.5-flash-lite',
+    'gemma-3-27b-it',
+    'gemma-3-12b-it',
+    'gemma-3-4b-it',
+    'gemma-3-1b-it',
+    'gemma-3n-e4b-it',
+    'gemma-3n-e2b-it'
   ]
 
-  const model = useCookie<string>('model', { default: () => 'gemini-2.5-pro' })
+  const model = useCookie<string>('model', { default: () => 'gemini-2.5-flash-lite' })
 
   return {
     models,
