8b9e2722f63c9857542a7b69d309d816f1a77efb
livisghton <livisghtonaraujo@gmail.com>
2025-12-02 09:44:12 -0300

    feat: adiciona os layout do figma

--- app/pages/auth/login.vue
+++ app/pages/auth/login.vue
@@ -5,10 +5,20 @@ definePageMeta({
 </script>
 
 <template>
-  <AuthPage>
+  <AuthPage style="background-image: url('/bg-login.png');" class="bg-cover">
     <AuthLogin>
       <template #description>
-        Ainda não tem conta? <ULink :to="{ name: 'auth-register' }">Registre-se</ULink>.
+        <div class="grid grid-flow-row place-items-center gap-2">
+          <img src="/icon-atlas-branco.svg" alt="Logo" class="h-20" />
+
+          <h1 class="text-3xl font-semibold text-white">
+            Bem-vindo ao <span class="text-teal-500">atlas</span>
+          </h1>
+
+          <p class="text-sm text-white/60">
+            Entre com seu e-mail e senha
+          </p>
+        </div>
       </template>
     </AuthLogin>
   </AuthPage>
