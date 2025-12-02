e22a51643befec486bcc851da53eb25f9682543a
livisghton <livisghtonaraujo@gmail.com>
2025-12-02 15:54:45 -0300

    feat: improved login page (#2)

--- app/pages/auth/login.vue
+++ app/pages/auth/login.vue
@@ -1,14 +1,25 @@
 <script setup lang="ts">
 definePageMeta({
-  layout: 'auth'
+  layout: 'auth',
+  colorMode: 'light'
 })
 </script>
 
 <template>
-  <AuthPage>
+  <AuthPage style="background-image: url('/bg-login.png');" class="bg-cover">
     <AuthLogin>
       <template #description>
-        Ainda não tem conta? <ULink :to="{ name: 'auth-register' }">Registre-se</ULink>.
+        <div class="grid grid-flow-row place-items-center gap-2">
+          <img src="/icon-atlas-branco.svg" alt="Logo" class="h-20" />
+
+          <h1 class="text-3xl font-semibold text-white">
+            Bem-vindo ao <span class="text-teal-500">atlas</span>
+          </h1>
+
+          <p class="text-sm text-white/60">
+            Entre com seu e-mail e senha
+          </p>
+        </div>
       </template>
     </AuthLogin>
   </AuthPage>
